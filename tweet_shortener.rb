ca7255424a8a320cf033dba4b091b30606caadf8
Robert Tate <btate712@gmail.com>
2019-03-03 16:19:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def dictionary(word)
+  substitutes = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&" }
+
+  if(substitutes.include?(word))
+    return substitutes[word]
+  else
+    return word
+  end
+end
+
+puts dictionary("too")
+puts dicitonary("hello")
+
+  
